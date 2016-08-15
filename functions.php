f2655190a0de4b95391d43218c0b406df5a264ac
Juremy1234 <Juremyseochand@gmail.com>
2016-08-15 16:00:53 -0400

    Called my options page

--- functions.php
+++ functions.php
@@ -138,3 +138,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load load options file.
+ */
+require get_stylesheet_directory() . '/inc/options.php';
